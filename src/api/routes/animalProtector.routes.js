7525294f7cdcc95f304684857527f8c93e9c4a67
Hernan <hschiavonr@gmail.com>
2022-11-25 12:49:33 +0100

    fix put v2

--- src/api/routes/animalProtector.routes.js
+++ src/api/routes/animalProtector.routes.js
@@ -1,15 +1,17 @@
 const express = require("express"); 
+const upload = require('../../middlewares/upload.file');
+const router = express.Router();
 
-const router = express.Router ();
 
 
-const {getAllAnimalProtector, postNewAnimalProtector,putAnimalProtector} = require("../controllers/animalProtector.controllers");
+
+const {getAllAnimalProtector, postNewAnimalProtector,putAnimalProtector,registerProtector,loginProtector,logoutProtector} = require("../controllers/animalProtector.controllers");
 
 router.get("/", getAllAnimalProtector);
-router.post("/", postNewAnimalProtector);
-router.put("/:id",putAnimalProtector)
-
-
-
+router.post("/",upload.fields([{name:'photo', maxCount: 1}]), postNewAnimalProtector);
+router.put("/:id",upload.fields([{name:'photo', maxCount: 1}]),putAnimalProtector)
+router.post("/registerProtector", registerProtector)
+router.post("/loginProtector", loginProtector)
+router.post('/logoutProtector', logoutProtector)
 
 module.exports = router;
